e2d7d0fda4197a72a5efa91edc102d0e4afb5a39
Sadunikaku <kulathilakasandunika@gmail.com>
2023-12-07 11:47:32 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center p-2 m-0 border-bottom">
+          React with AJAX
+      </h1>
+
+    </>
+  )
+}
+
+export default App
